92c4ff4427bd89ef1aaf2b629eaa22a0193a204b
David Lam <david@buddybuild.com>
2017-05-23 11:35:00 -0700

    update postclone script for develop branch

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ "$SUPER_SECRET_ENV_VAR" != "thisismysecret" ]]; then
-  echo "SUPER_SECRET_ENV_VAR had no value or was set to the wrong value"
-  echo "Aborting build"
-  exit 1
-else
-  echo "Found SUPER_SECRET_ENV_VAR!"
+if [[ "$BUDDYBUILD_BRANCH" == "develop" ]]; then
+    if [[ "$SUPER_SECRET_ENV_VAR" != "thisismysecretforevelop" ]]; then
+      echo "SUPER_SECRET_ENV_VAR had no value or was set to the wrong value for the develop branch"
+      echo "Aborting build"
+      exit 1
+    else
+      echo "Found SUPER_SECRET_ENV_VAR for develop branch!"
+    fi
 fi
