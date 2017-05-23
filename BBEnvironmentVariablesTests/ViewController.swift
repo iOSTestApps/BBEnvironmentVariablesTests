7d25d371916807653e9b0a74572b1a50dec30103
David Lam <david@buddybuild.com>
2017-05-23 10:40:52 -0700

    first commit

---
+++ BBEnvironmentVariablesTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBEnvironmentVariablesTests
+//
+//  Created by David Lam on 2017-05-23.
+//  Copyright © 2017 buddbuild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
